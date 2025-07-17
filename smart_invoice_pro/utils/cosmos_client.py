d3b9064585e49ccd1ba9c06dd8438219651d6e67
Davinder Pal <davinder@solidevelectrosoft.com>
2025-07-17 10:45:32 +0530

    -Deploy

---
+++ smart_invoice_pro/utils/cosmos_client.py
@@ -0,0 +1,31 @@
+import os
+from azure.cosmos import CosmosClient, PartitionKey, exceptions
+from dotenv import load_dotenv
+
+load_dotenv()
+
+uri = os.getenv("COSMOS_URI")
+key = os.getenv("COSMOS_KEY")
+database_name = os.getenv("COSMOS_DB_NAME")
+
+client = CosmosClient(uri, credential=key)
+database = client.create_database_if_not_exists(id=database_name)
+
+def get_container(container_name, partition_key):
+    try:
+        # Try to get the container if it exists
+        return database.get_container_client(container_name)
+    except exceptions.CosmosResourceNotFoundError:
+        # If not found, create it with the correct partition key
+        return database.create_container(
+            id=container_name,
+            partition_key=PartitionKey(path=partition_key),
+            offer_throughput=400
+        )
+
+users_container = get_container("users", "/userid")
+invoices_container = get_container("invoices", "/customer_id")
+customers_container = get_container("customers", "/customer_id")
+products_container = get_container("products", "/product_id")
+stock_container = get_container("stock", "/product_id")
+
